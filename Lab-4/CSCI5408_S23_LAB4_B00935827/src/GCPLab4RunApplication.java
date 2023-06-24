215e0f6c7e20ac809acfe6adea291aecec1ab5f5
Bhavisha Oza <bh327148@dal.ca>
2023-06-24 20:57:33 -0300

    Update Lab-4/CSCI5408_S23_LAB4_B00935827/src/GCPLab4RunApplication.java

--- Lab-4/CSCI5408_S23_LAB4_B00935827/src/GCPLab4RunApplication.java
+++ Lab-4/CSCI5408_S23_LAB4_B00935827/src/GCPLab4RunApplication.java
@@ -8,22 +8,24 @@ public class GCPLab4RunApplication {
 
             Statement statement = connection.createStatement();
 
-            // Query-1: SELECT query to fetch item details
+            // Execute a SELECT query to fetch item details from the inventory table
             String sqlQuery = "SELECT * FROM inventory";
             ResultSet resultSet = statement.executeQuery(sqlQuery);
 
+            // Process the result set
             while (resultSet.next()) {
                 int itemId = resultSet.getInt("item_id");
                 String itemName = resultSet.getString("item_name");
                 int availableQuantity = resultSet.getInt("available_quantity");
 
-                System.out.println("*************************");
-                System.out.println("Item ID is: " + itemId);
-                System.out.println("Item Name is: " + itemName);
-                System.out.println("Available Quantity is: " + availableQuantity);
-                System.out.println("*************************");
+                // Print the item details
+                System.out.println("Item ID: " + itemId);
+                System.out.println("Item Name: " + itemName);
+                System.out.println("Available Quantity: " + availableQuantity);
+                System.out.println("--------------------");
             }
 
+            // Close the resources
             resultSet.close();
             statement.close();
             connection.close();
@@ -38,12 +40,24 @@ public class GCPLab4RunApplication {
             // Establish a connection to the local database
             Connection connection = DBConnection.getLocalDBConnection();
 
+            // Create a statement object to execute SQL queries
             Statement statement = connection.createStatement();
 
+            // Execute a CREATE TABLE query to create the order_info table
+            String createTableQuery = "CREATE TABLE orders ("
+                    + "order_id INT AUTO_INCREMENT PRIMARY KEY,"
+                    + "user_id INT,"
+                    + "item_name VARCHAR(255),"
+                    + "quantity INT,"
+                    + "order_date DATE)";
+            statement.executeUpdate(createTableQuery);
+
+            System.out.println("Order table created successfully!");
 
             // Update the available quantity in the remote database
             updateRemoteInventory(connection);
 
+            // Close the resources
             statement.close();
             connection.close();
         } catch (SQLException e) {
@@ -53,14 +67,13 @@ public class GCPLab4RunApplication {
     public static void updateRemoteInventory(Connection connection) {
         try {
             Connection remoteconnection = DBConnection.getRemoteDBConnection();
-            
+            // Create a statement object to execute SQL queries
             Statement statement = remoteconnection.createStatement();
-
             // Execute an UPDATE query to update the available quantity in the remote inventory table
             String updateQuery = "UPDATE inventory SET available_quantity = available_quantity - 1 where item_id=1";
             statement.executeUpdate(updateQuery);
 
-            System.out.println("Yay... Updated in the remote inventory!!!!!!");
+            System.out.println("Updated quantity in the remote inventory!");
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
