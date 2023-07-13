faf80f3f01a781376c28d68c4703dda7c74006ec
boza <bh327148@dal.ca>
2023-07-12 21:02:08 -0300

    Attempt to create ERD.

--- A2/src/main/java/database/ERDGenerator.java
+++ A2/src/main/java/database/ERDGenerator.java
@@ -4,24 +4,41 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * The ERDGenerator class generates an Entity-Relationship Diagram (ERD) based on the data and structure files.
+ */
 public class ERDGenerator {
     private String dataFilePath;
     private String structureFilePath;
 
+    /**
+     * Constructor for ERDGenerator object with the specified data and structure file paths.
+     *
+     * @param dataFilePath      The path to the data file.
+     * @param structureFilePath The path to the structure file.
+     */
     public ERDGenerator(String dataFilePath, String structureFilePath) {
         this.dataFilePath = dataFilePath;
         this.structureFilePath = structureFilePath;
     }
 
+    /**
+     * Generates the Entity-Relationship Diagram (ERD) based on the data and structure files.
+     */
     public void generateERD() {
+        // Read table columns and table data
         Map<String, List<String>> tableColumns = readTableColumns();
         Map<String, List<String>> tableData = readTableData();
 
+        // Print the header for the ERD
         System.out.println("--------------------------------------------------");
         System.out.println("Entity-Relationship Diagram");
         System.out.println("--------------------------------------------------");
 
+        // Print entity representation for each table
         for (String tableName : tableColumns.keySet()) {
             System.out.println(tableName);
             List<String> columns = tableColumns.get(tableName);
@@ -56,6 +73,11 @@ public class ERDGenerator {
         System.out.println("--------------------------------------------------");
     }
 
+    /**
+     * Reads the table columns from the structure file.
+     *
+     * @return A map containing table names as keys and lists of column names as values.
+     */
     private Map<String, List<String>> readTableColumns() {
         Map<String, List<String>> tableColumns = new HashMap<>();
 
@@ -79,6 +101,11 @@ public class ERDGenerator {
         return tableColumns;
     }
 
+    /**
+     * Reads the table data from the data file.
+     *
+     * @return A map containing table names as keys and lists of data rows as values.
+     */
     private Map<String, List<String>> readTableData() {
         Map<String, List<String>> tableData = new HashMap<>();
 
@@ -102,6 +129,12 @@ public class ERDGenerator {
         return tableData;
     }
 
+    /**
+     * Retrieves the primary keys from a list of columns.
+     *
+     * @param columns The list of column names.
+     * @return A list of primary key column names.
+     */
     private List<String> getPrimaryKeys(List<String> columns) {
         List<String> primaryKeys = new ArrayList<>();
         for (String column : columns) {
@@ -112,6 +145,11 @@ public class ERDGenerator {
         return primaryKeys;
     }
 
+    /**
+     * Reads the relationships between tables from the structure file.
+     *
+     * @return A list of relationships in the format "Table1 -> Table2".
+     */
     private List<String> readRelationships() {
         List<String> relationships = new ArrayList<>();
 
@@ -120,8 +158,13 @@ public class ERDGenerator {
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
                 if (line.startsWith("ALTER TABLE") && line.contains("FOREIGN KEY")) {
-                    String relationship = line.replaceAll("ALTER TABLE `(\\w+)`.*REFERENCES `(\\w+)`.*", "$1 -> $2");
-                    relationships.add(relationship);
+                    Pattern pattern;
+                    pattern = Pattern.compile("ALTER TABLE `(\\w+)`.*REFERENCES `(\\w+)`.*");
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        String relationship = matcher.group(1) + " -> " + matcher.group(2);
+                        relationships.add(relationship);
+                    }
                 }
             }
         } catch (IOException e) {
